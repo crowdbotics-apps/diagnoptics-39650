6e91b716d4aa839eb19ff81923d0bdf760b8a893
Genevieve Springer <genevieve@renoptics.io>
2023-04-01 02:03:13 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "diagnoptics-39650",
   "url": "https://diagnoptics-39650.botics.co",
   "api": "https://diagnoptics-39650.botics.co/api/v1",
-  "initialRoute": "ArchiveContentScreen5"
+  "initialRoute": "BlankScreen18297"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
